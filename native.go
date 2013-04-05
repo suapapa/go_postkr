8218922f6ee63790480ff9d530389397c961b5b2
Homin Lee <homin.lee@suapapa.net>
2013-04-05 15:58:35 +0900

    Use go-charset to en/decode euc-kr
    The go-charset is pure go replacement for go-iconv.
    It doesn't support euc-kr but support cp949 which is superset.

--- native.go
+++ native.go
@@ -2,24 +2,37 @@ package postkr
 
 import (
 	"bytes"
+	"code.google.com/p/go-charset/charset"
+	_ "code.google.com/p/go-charset/data"
 	"encoding/xml"
 	"errors"
-	iconv "github.com/djimenez/iconv-go"
 	"io"
 )
 
 func unmarshalCp949XML(data []byte, v interface{}) error {
 	d := xml.NewDecoder(bytes.NewBuffer(data))
 	d.CharsetReader = func(c string, i io.Reader) (io.Reader, error) {
-		if c != "euc-kr" && c != "cp949" {
+		if c != "cp949" && c != "euc-kr" {
 			return nil, errors.New("unexpect charset: " + c)
 		}
-		return iconv.NewReader(i, "cp949", "utf8")
+		tr, err := charset.TranslatorFrom("cp949")
+		if err != nil {
+			panic(err)
+		}
+		return charset.NewTranslatingReader(i, tr), nil
 	}
 
 	return d.Decode(v)
 }
 
 func encodeToCp949(utf8Str string) (string, error) {
-	return iconv.ConvertString(utf8Str, "utf8", "cp949")
+	tr, err := charset.TranslatorTo("cp949")
+	if err != nil {
+		return "", err
+	}
+	_, r, err := tr.Translate([]byte(utf8Str), false)
+	if err != nil {
+		return "", err
+	}
+	return string(r), nil
 }
